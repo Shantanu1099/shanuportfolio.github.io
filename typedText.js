c2072a647192cb992429eecbe3ed08edf4f77d45
Shantanu Singh <38537606+Shantanu1099@users.noreply.github.com>
2020-11-27 03:01:54 +0530

    Add files via upload

---
+++ typedText.js
@@ -0,0 +1,19 @@
+var typed = new Typed ('.head-typer',{
+    strings: ["Shantanu Singh^1000 ",
+"<em>a Developer&nbsp; &amp;</em>",
+"a Problem Solver .",
+],
+    typeSpeed: 35,
+    backSpeed: 50,
+    loop: true,
+});
+
+var typeD = new Typed ('.about-msg',{
+    strings: ["<strong>Trying to Learn most of the things in this world to feed my curiosity .^3000 </strong>",
+    "<strong>who likes solving problems and is open to new opportunities .^1000 </strong>",
+    " &nbsp; ",
+],
+    typeSpeed: 25,
+    backSpeed: 40,
+    loop: true,
+});
